fc6ab68f664412470e088bacac0edcfe5a053b01
Unreal Dev <75167053+UnrealDev16@users.noreply.github.com>
2023-09-30 07:00:29 +0000

    Add JWT.js

---
+++ JWT.js
@@ -0,0 +1,50 @@
+const crypto = require('crypto')
+
+function base64UrlEncode(str) {
+  let base64 = Buffer.from(str).toString('base64')
+  return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '')
+}
+
+function createJWT(payload, expiresIn) {
+  const header = {
+    alg: 'HS256',
+    typ: 'JWT',
+  }
+
+  const issuedAt = Math.floor(Date.now() / 1000) // Issued at timestamp in seconds
+  const expirationTime = issuedAt + expiresIn // Expiration time in seconds
+
+  const updatedPayload = {
+    ...payload,
+    iat: issuedAt,
+    exp: expirationTime,
+  }
+
+  const encodedHeader = base64UrlEncode(JSON.stringify(header))
+  const encodedPayload = base64UrlEncode(JSON.stringify(updatedPayload))
+
+  const signature = crypto.createHmac('sha256', Bun.env.JWT_KEY).update(`${encodedHeader}.${encodedPayload}`).digest('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '')
+
+  return `${encodedHeader}.${encodedPayload}.${signature}`
+}
+
+function verifyJWT(jwt) {
+  const [encodedHeader, encodedPayload, receivedSignature] = jwt.split('.')
+
+  const signature = crypto.createHmac('sha256', Bun.env.JWT_KEY).update(`${encodedHeader}.${encodedPayload}`).digest('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '')
+
+  const decodedPayload = JSON.parse(Buffer.from(encodedPayload, 'base64').toString('utf-8'))
+
+  const currentTimestamp = Math.floor(Date.now() / 1000)
+
+  if (signature === receivedSignature && decodedPayload.exp >= currentTimestamp) {
+    return decodedPayload
+  } else {
+    return null
+  }
+}
+
+module.exports = {
+  createJWT,
+  verifyJWT,
+}
